fc63280f1d365a5a53b6941094dd260f424b287a
readerror <readev@gmail.com>
2013-04-24 18:10:30 -0400

    animations enabled for RamAI
    moved ram texture to texture folder

--- Assets/Scripts/RamAI.js
+++ Assets/Scripts/RamAI.js
@@ -4,6 +4,7 @@ var _smoke : ParticleEmitter;
 
 function Start () 
 {
+	animation.Play("Ram_Idle");
 
 }
 
@@ -16,11 +17,18 @@ function OnTriggerEnter(objColl : Collider)
 {
 	
 	if(objColl.gameObject.tag == "MovingWall") 
-	{		
-		_flameOne.enabled = true;
-		_flameTwo.enabled = true;
+	{	
+		animation.CrossFade("Ram_Aggro",.3);
 		_smoke.enabled = true;
+		
+		yield WaitForSeconds (2);
+		_flameTwo.enabled = true;	
+		
+		yield WaitForSeconds (1);	
+		_flameOne.enabled = true;
+		
 		GetComponent(PathFinder).speed = GetComponent(scriptActorEnemy).speed * 10;		
+		
 	}
 	
 }
