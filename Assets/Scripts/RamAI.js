2f4f4b27e385e077173ca2fe08531d2e38596448
readerror <readev@gmail.com>
2013-04-26 05:12:04 -0400

    HD crosshair and targets
    imported ram sound; not set up yet

--- Assets/Scripts/RamAI.js
+++ Assets/Scripts/RamAI.js
@@ -1,10 +1,14 @@
 var _flameOne : ParticleEmitter;
 var _flameTwo : ParticleEmitter;
 var _smoke : ParticleEmitter;
+private var sound  : AudioSource;
+var ramAggro : AudioClip;
 
 function Start () 
 {
 	animation.Play("Ram_Idle");
+	
+	sound = gameObject.GetComponent(AudioSource);
 
 }
 
@@ -18,6 +22,9 @@ function OnTriggerEnter(objColl : Collider)
 	
 	if(objColl.gameObject.tag == "MovingWall") 
 	{	
+		sound.clip = ramAggro;
+		sound.Play();
+		
 		animation.CrossFade("Ram_Aggro",.3);
 		_smoke.enabled = true;
 		
