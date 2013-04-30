837cbec20beaad9f474cbaba39604e835580c528
Josiah Leon Bahl <jobahl@epix.net>
2013-04-30 19:53:15 -0400

    More About Souls.
    I redid the soul particle effect and made it move towards a cube behind
    the GUI, when it reaches the cube it spawns an explosion particle
    effect.

--- Assets/Scripts/Movement.js
+++ Assets/Scripts/Movement.js
@@ -1,7 +1,25 @@
 var _speed : int = 1;
 var _lifeSpan : int = 3;
 
-function Update () {
-	transform.Translate(0,1*Time.deltaTime, _speed);
-	Destroy(gameObject, _lifeSpan);
+var _soulFocus : GameObject;
+var _soulExplosion : GameObject;
+
+function Start()
+{
+	_soulFocus = GameObject.FindGameObjectWithTag("SoulFocus");
+}
+
+function Update () 
+{
+	var speed = _speed * Time.deltaTime;
+	transform.position = Vector3.MoveTowards(transform.position, _soulFocus.transform.position, speed);
+}
+
+function OnCollisionEnter(other : Collision)
+{
+	if(other.gameObject.tag == "SoulFocus")
+	{
+		Instantiate(_soulExplosion, this.transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
 }
