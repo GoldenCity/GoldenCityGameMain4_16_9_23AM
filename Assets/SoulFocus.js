b9e4c4fb1fc3816f77b3c7dec9e1e3537bb77829
readerror <readev@gmail.com>
2013-05-01 17:16:34 -0400

    SoulFocus.js now only checks 1 time per second

--- Assets/SoulFocus.js
+++ Assets/SoulFocus.js
@@ -9,14 +9,24 @@ var _zAdjust : int = 0;
 
 function Start () {
 	_zAdjust = transform.position.z;
+	
+	InvokeRepeating("SetFocus", 0, 1); //only check 1 time every second
 }
 
-function Update () {
-	//based on the "Souls Collected: " GUI Label placement in Script_Hud
-	var p : Vector3 = _camera.ScreenToWorldPoint (Vector3( 
-													Screen.width - (Screen.width / (4.0 + (_xAdjust / 10.0) ) ), 
-													Screen.height - (Screen.height / (80.0) + _yAdjust), 
-													_zAdjust) );
-													
-	transform.position = p;
+
+private var screenSize = Vector2(0,0); //store for screen size
+
+function SetFocus () {
+	if (screenSize != Vector2(Screen.width, Screen.height) ) {
+
+		//based on the "Souls Collected: " GUI Label placement in Script_Hud
+		var p : Vector3 = _camera.ScreenToWorldPoint (Vector3( 
+														Screen.width - (Screen.width / (4.0 + (_xAdjust / 10.0) ) ), 
+														Screen.height - (Screen.height / (80.0) + _yAdjust), 
+														_zAdjust) );
+														
+		transform.position = p;
+		
+		screenSize = Vector2(Screen.width, Screen.height);
+	}
 }
