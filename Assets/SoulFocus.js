c225fdf9a8c2e0f8a5f609ae01abed94b1c03745
readerror <readev@gmail.com>
2013-05-01 06:19:48 -0400

    set up SoulFocus object and script
    material for gate. w or w.o.
    projectile target texture projectors ignore IgnoreRayCast layer

--- Assets/SoulFocus.js
+++ Assets/SoulFocus.js
@@ -1,5 +1,7 @@
 #pragma strict
 
+//Script controls placement of SoulFocus relative to screen so it can be lined up whatever the screen size or aspect
+
 var _camera  : Camera;
 var _xAdjust : int = 0;
 var _yAdjust : int = 0;
@@ -11,10 +13,10 @@ function Start () {
 
 function Update () {
 	//based on the "Souls Collected: " GUI Label placement in Script_Hud
-	var p : Vector3 = _camera.ScreenToWorldPoint (Vector3 ( Screen.width-(Screen.width/(4.0+_xAdjust)), Screen.height-(Screen.height/(80.0)+_yAdjust), _zAdjust));
-	print(p);
+	var p : Vector3 = _camera.ScreenToWorldPoint (Vector3( 
+													Screen.width - (Screen.width / (4.0 + (_xAdjust / 10.0) ) ), 
+													Screen.height - (Screen.height / (80.0) + _yAdjust), 
+													_zAdjust) );
+													
 	transform.position = p;
-	
-
-
 }
