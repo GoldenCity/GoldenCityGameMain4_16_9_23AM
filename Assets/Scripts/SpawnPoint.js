d7de1b66c0bc52325dc36b430225541cc5094a60
Jeff Smallwood <jeff.w.smallwood@gmail.com>
2013-05-04 20:53:12 -0400

    Fix flight plan again
    reattach some things that came unattached in last commit

--- Assets/Scripts/SpawnPoint.js
+++ Assets/Scripts/SpawnPoint.js
@@ -1,7 +1,17 @@
 #pragma strict
 
 var valid : boolean;
+var timeOff : float = 0;
 
 function Start () {
 	valid = true;
+}
+
+function Update() {
+	if (valid == false) {
+		timeOff += Time.deltaTime;
+	}
+	if (timeOff > 4) {
+		valid = true;
+	}
 }
