3a63364da995a1dbb96989dbf39fec8c9202cd9c
readerror <readev@gmail.com>
2013-05-13 18:07:38 -0400

    continued work on wheel animator. it's trickier than expected

--- Assets/Scripts/wheelAnimator.js
+++ Assets/Scripts/wheelAnimator.js
@@ -3,31 +3,42 @@
 var d : float; //wheel diameter
 var c : float; //circumference
 var side : int;
+
+var manticore : boolean = false;
 function Start () {
 	GetDia();
-	GetCirc();
+	
 	GetSide();
 }
 
 function Update () {
-	var rot = gameObject.transform.position.z % 360;
-gameObject.transform.Rotate(side * rot, 0, 0);	
+	if(c != 0 && c != null) {
+		var rot = gameObject.transform.position.z % c;
+		rot = (rot/c)*360;
+		if(!manticore)
+			gameObject.transform.Rotate(side * rot, 0, 0);	
+		else
+			gameObject.transform.Rotate(0, -side * rot, 0);	
+	}
 
 }
 
 function GetDia () {
 	yield;	//wait because they may be scaled when first created
-	d = renderer.bounds.size.z;
+
+		d = renderer.bounds.size.z;
+	
+	GetCirc();
+
 }
 
 function GetCirc () {
-	yield;
 	c = Mathf.PI*d;
 }
 
 function GetSide() {
 //	print(gameObject.transform.localRotation.eulerAngles.y +"--"+ gameObject.transform.parent.rotation.eulerAngles.y);
-	if(gameObject.transform.localPosition.x > .1)
+	if(gameObject.transform.localPosition.x > -.1)
 		side = 1;
 	else
 		side = -1;
