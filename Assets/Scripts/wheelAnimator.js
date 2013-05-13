cd24e764af9bd200a0b0b1bbe57d8d7234b18435
readerror <readev@gmail.com>
2013-05-13 17:25:16 -0400

    working on wheel animator

--- Assets/Scripts/wheelAnimator.js
+++ Assets/Scripts/wheelAnimator.js
@@ -2,14 +2,16 @@
 
 var d : float; //wheel diameter
 var c : float; //circumference
-
+var side : int;
 function Start () {
 	GetDia();
 	GetCirc();
+	GetSide();
 }
 
 function Update () {
-	
+	var rot = gameObject.transform.position.z % 360;
+gameObject.transform.Rotate(side * rot, 0, 0);	
 
 }
 
@@ -19,5 +21,14 @@ function GetDia () {
 }
 
 function GetCirc () {
+	yield;
 	c = Mathf.PI*d;
+}
+
+function GetSide() {
+//	print(gameObject.transform.localRotation.eulerAngles.y +"--"+ gameObject.transform.parent.rotation.eulerAngles.y);
+	if(gameObject.transform.localPosition.x > .1)
+		side = 1;
+	else
+		side = -1;
 }
